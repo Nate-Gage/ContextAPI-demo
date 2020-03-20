4e6999f4a883362ed6d0c60f5fc2bd3ea0e2650e
Nate <nate.d.gage@gmail.com>
2020-03-20 12:24:16 -0600

    added delete button for individual book items

--- src/Book.js
+++ src/Book.js
@@ -5,6 +5,10 @@ const Book = (props) => {
     return (
         <div>
             <h3>{props.name}, <span className="bookPrice">$ {props.price}</span></h3>
+            <button onClick={() => {
+                props.removeBook(props.book.name)
+            }}
+            >X</button>
         </div>
     );
 };
--- src/BookList.js
+++ src/BookList.js
@@ -6,10 +6,16 @@ const BookList = () => {
     //const value is derived from value in BooksContext
     const [books, setBooks] = useContext(BooksContext);
 
+    const removeBook = (name) => {
+        setBooks(books.filter((book) => book.name != name))
+    };
+
+    //the mapped Book component needs the individual book={book} prop in order to
+    //delete related item from list
     return (
         <div>
             {books.map(book => (
-                <Book name={book.name} price={book.price} key={book.id} />
+                <Book book={book} name={book.name} price={book.price} key={book.id} removeBook={removeBook} />
             ))}
         </div>
     );
